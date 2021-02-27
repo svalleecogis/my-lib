d63d2c56f4cc9d2e67e554533c95ec504f9fe51c
Gogs <gogs@fake.local>
2021-02-27 18:16:36 +0100

    first commit

---
+++ projects/my-lib/src/lib/my-lib.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import concat from 'lodash.concat';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyLibService {
+
+  constructor() { }
+
+
+  doSomething() {
+    // Make sure tree shaking won't remove the lib during the build
+    console.log("concat...............",concat([1], 2))
+  }
+}
